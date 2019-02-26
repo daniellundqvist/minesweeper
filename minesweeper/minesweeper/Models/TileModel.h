fa0dad472f713d8cb30afc329c72bce690510fd2
Daniel Lundqvist <daniellundqvist77@gmail.com>
2019-02-26 13:59:11 +0100

    add a tile model

---
+++ minesweeper/minesweeper/Models/TileModel.h
@@ -0,0 +1,28 @@
+//
+//  TileModel.h
+//  minesweeper
+//
+//  Created by Daniel Lundqvist on 2019-02-26.
+//  Copyright © 2019 Daniel Lundqvist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSUInteger, TileState) {
+    TileStateNoMine,
+    TileStateMine,
+    TileStateProtected,
+};
+
+@interface TileModel : NSObject
+
+@property (nonatomic) TileState tileState;
+@property (nonatomic) BOOL flagged;
+@property (nonatomic) BOOL turned;
+@property (nonatomic) NSInteger mineCount;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ minesweeper/minesweeper/Models/TileModel.m
@@ -0,0 +1,13 @@
+//
+//  TileModel.m
+//  minesweeper
+//
+//  Created by Daniel Lundqvist on 2019-02-26.
+//  Copyright © 2019 Daniel Lundqvist. All rights reserved.
+//
+
+#import "TileModel.h"
+
+@implementation TileModel
+
+@end
