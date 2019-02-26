c5bfe6d18d66ba42f1d793368b7727a4922b64bc
Daniel Lundqvist <daniellundqvist77@gmail.com>
2019-02-26 13:30:25 +0100

    add and connect ui elements in storyboard

--- minesweeper/minesweeper/ViewController.m
+++ minesweeper/minesweeper/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+
+@property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (strong, nonatomic) IBOutlet UIButton *replayButton;
+@property (strong, nonatomic) IBOutlet UILabel *mineCounterLabel;
+@property (strong, nonatomic) IBOutlet UILabel *timerLabel;
 
 @end
 
@@ -16,7 +21,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 9;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 9;
+}
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"tile" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+#pragma mark - UICollectionViewFlowLayout Delegate
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CGFloat tileSize = (self.view.frame.size.width / self.collectionView.numberOfSections);
+    return CGSizeMake(tileSize, tileSize);
+}
+
+#pragma mark - Action Methods
+
+- (IBAction)replayButtonTapped:(id)sender {
+    
 }
 
 
